b25e03a27211e6b7eb3634abe29ce6e86ee96a84
Steven Maude <git@stevenmaude.co.uk>
2025-12-12 18:27:09 +0000

    Reformat with `ruff`

--- scraperwiki/__init__.py
+++ scraperwiki/__init__.py
@@ -1,7 +1,8 @@
-'''
+"""
 Local version of ScraperWiki Utils, documentation here:
 https://scraperwiki.com/docs/python/python_help_documentation/
-'''
+"""
+
 from .utils import scrape, pdftoxml, status, swimport
 from . import utils
 from . import sql
@@ -10,19 +11,23 @@ from . import sql
 sqlite = sql
 
 __all__ = [
-        'scrape',
-        'pdftoxml',
-        'status',
-        'swimport',
-        'utils',
-        'sql',
+    "scrape",
+    "pdftoxml",
+    "status",
+    "swimport",
+    "utils",
+    "sql",
 ]
+
 
 class Error(Exception):
     """All ScraperWiki exceptions are instances of this class
     (usually via a subclass)."""
+
     pass
+
 
 class CPUTimeExceededError(Error):
     """CPU time limit exceeded."""
+
     pass
--- scraperwiki/utils.py
+++ scraperwiki/utils.py
@@ -1,7 +1,8 @@
-'''
+"""
 Local version of ScraperWiki Utils, documentation here:
 https://scraperwiki.com/docs/python/python_help_documentation/
-'''
+"""
+
 import os
 import shutil
 import warnings
@@ -11,19 +12,19 @@ import urllib.request
 
 
 def scrape(url, params=None, user_agent=None):
-    '''
+    """
     Scrape a URL optionally with parameters.
     This is effectively a wrapper around urllib.request.urlopen.
-    '''
+    """
 
     headers = {}
 
     if user_agent:
-        headers['User-Agent'] = user_agent
+        headers["User-Agent"] = user_agent
 
     data = None
     if params:
-        data = urllib.parse.urlencode(params).encode('utf-8')
+        data = urllib.parse.urlencode(params).encode("utf-8")
 
     req = urllib.request.Request(url, data=data, headers=headers)
 
@@ -35,27 +36,28 @@ def scrape(url, params=None, user_agent=None):
 
 def pdftoxml(pdfdata, options=""):
     """converts pdf file to xml file"""
-    if not shutil.which('pdftohtml'):
+    if not shutil.which("pdftohtml"):
         warnings.warn(
-            'scraperwiki.pdftoxml requires pdftohtml, but pdftohtml was not found '
-            'in the PATH. If you wish to use this function, you probably need to '
-            'install pdftohtml.'
+            "scraperwiki.pdftoxml requires pdftohtml, but pdftohtml was not found "
+            "in the PATH. If you wish to use this function, you probably need to "
+            "install pdftohtml."
         )
         return None
-    pdffout = tempfile.NamedTemporaryFile(suffix='.pdf')
+    pdffout = tempfile.NamedTemporaryFile(suffix=".pdf")
     pdffout.write(pdfdata)
     pdffout.flush()
 
-    xmlin = tempfile.NamedTemporaryFile(mode='r', suffix='.xml', encoding="utf-8")
+    xmlin = tempfile.NamedTemporaryFile(mode="r", suffix=".xml", encoding="utf-8")
     tmpxml = xmlin.name  # "temph.xml"
     cmd = 'pdftohtml -xml -nodrm -zoom 1.5 -enc UTF-8 -noframes {} "{}" "{}"'.format(
-        options, pdffout.name, os.path.splitext(tmpxml)[0])
+        options, pdffout.name, os.path.splitext(tmpxml)[0]
+    )
     # can't turn off output, so throw away even stderr yeuch
     cmd = cmd + " >/dev/null 2>&1"
     os.system(cmd)
 
     pdffout.close()
-    #xmlfin = open(tmpxml)
+    # xmlfin = open(tmpxml)
     xmldata = xmlin.read()
     xmlin.close()
     return xmldata
@@ -63,7 +65,11 @@ def pdftoxml(pdfdata, options=""):
 
 def status(type, message=None):
     """Retained for backwards compatibility."""
-    warnings.warn("status() is no longer in use following ScraperWiki/Quickcode application shutdown", DeprecationWarning, stacklevel=2)
+    warnings.warn(
+        "status() is no longer in use following ScraperWiki/Quickcode application shutdown",
+        DeprecationWarning,
+        stacklevel=2,
+    )
     return
 
 
